bc46cf62282343c923ebc59ba75a4b948ac53e61
ducrz <ducatolico@gmail.com>
2022-04-29 16:09:27 -0300

    Fim Curso: Introdução ao JavaScript.

---
+++ Javascript/Introducao-ao-Javascript/funcoes.js
@@ -0,0 +1,11 @@
+// Declarando Função
+function soma(a, b) {
+
+    // console.log(a + b);
+
+    return a + b;
+}
+
+//Chamando Função
+
+soma(3, 5);
---
+++ Javascript/Introducao-ao-Javascript/teste.js
@@ -0,0 +1,27 @@
+// var y = 0; Atalho CTRL + /
+
+var x = 1;
+
+/* Comentário
+de mais de uma linha */
+
+
+var y = 0;
+var x = 1;
+
+var z = 2;
+
+
+//Variáveis
+
+//var preco = 2;
+var desconto = 0.2;
+
+const PRECO = 2;
+
+var total = PRECO - desconto;
+
+
+
+
+
