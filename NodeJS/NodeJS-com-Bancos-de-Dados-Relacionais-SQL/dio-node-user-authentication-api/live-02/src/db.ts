bc08d07b76ee6884fcb5c1331dc0ec5b310e85de
ducrz <ducatolico@gmail.com>
2022-05-11 21:36:58 -0300

    Fim Curso: Node.js com Bancos de Dados

--- NodeJS/NodeJS-com-Bancos-de-Dados-Relacionais-SQL/dio-node-user-authentication-api/live-02/src/db.ts
+++ NodeJS/NodeJS-com-Bancos-de-Dados-Relacionais-SQL/dio-node-user-authentication-api/live-02/src/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from 'pg';
 
-const connectionString = 'postgres://hxyfiadg:wW_IE5G_zq7TEvHvs7f_VMIdQWbaEsaZ@motty.db.elephantsql.com/hxyfiadg';
+const connectionString = 'URLEDITADA';
 
 const db = new Pool({ connectionString });
 
